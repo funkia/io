6900f1a4f50595f41c1d33d4337bbb941937b5f9
paldepind <simonfv@gmail.com>
2017-05-20 17:26:31 +0200

    Initial commit. Setup config and copy paste from Jabz

---
+++ src/index.ts
@@ -0,0 +1,153 @@
+import { Freer, liftF } from "./freer";
+import { Monad, AbstractMonad } from "@funkia/jabz";
+
+function deepEqual(a: any, b: any): boolean {
+  if (typeof a === "object" && typeof b === "object") {
+    const aKeys = Object.keys(a);
+    for (const key of aKeys) {
+      if (!deepEqual(a[key], b[key])) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return a === b;
+  }
+}
+
+export type F0<Z> =
+  () => Z;
+export type F1<A, Z> =
+  (a: A) => Z;
+export type F2<A, B, Z> =
+  (a: A, b: B) => Z;
+export type F3<A, B, C, Z> =
+  (a: A, b: B, c: C) => Z;
+export type F4<A, B, C, D, Z> =
+  (a: A, b: B, c: C, d: D) => Z;
+export type F5<A, B, C, D, E, Z> =
+  (a: A, b: B, c: C, d: D, e: E) => Z;
+
+export type IOValue<A> = Call | CallP | ThrowE | CatchE;
+
+export class Call {
+  type: "call" = "call";
+  constructor(public fn: Function, public args: any[]) { }
+}
+
+export class CallP {
+  type: "callP" = "callP";
+  constructor(public fn: Function, public args: any[]) { }
+}
+
+export class ThrowE {
+  type: "throwE" = "throwE";
+  constructor(public error: any) { }
+}
+
+export class CatchE {
+  type: "catchE" = "catchE";
+  constructor(public handler: (error: any) => IO<any>, public io: IO<any>) { }
+}
+
+export type IO<A> = Freer<IOValue<any>, A>;
+
+export const IO = Freer;
+
+// in the IO monad
+export function withEffects<A, Z>(f: F1<A, Z>): (a: A) => IO<Z>;
+export function withEffects<A, B, Z>(f: F2<A, B, Z>): (a: A, b: B) => IO<Z>;
+export function withEffects<A, B, C, Z>(f: F3<A, B, C, Z>): (a: A, b: B, c: C) => IO<Z>;
+export function withEffects<A, B, C, D, Z>(f: F4<A, B, C, D, Z>): (a: A, b: B, c: C, d: D) => IO<Z>;
+export function withEffects<A, B, C, D, E, Z>(f: F5<A, B, C, D, E, Z>): (a: A, b: B, c: C, d: D, e: E) => IO<Z>;
+export function withEffects<A>(fn: any): (...as: any[]) => IO<A> {
+  return (...args: any[]) => liftF(new Call(fn, args));
+}
+
+export function withEffectsP<A, Z>(f: F1<A, Promise<Z>>): (a: A) => IO<Z>;
+export function withEffectsP<A, B, Z>(f: F2<A, B, Promise<Z>>): (a: A, b: B) => IO<Z>;
+export function withEffectsP<A, B, C, Z>(f: F3<A, B, C, Promise<Z>>): (a: A, b: B, c: C) => IO<Z>;
+export function withEffectsP<A, B, C, D, Z>(f: F4<A, B, C, D, Promise<Z>>): (a: A, b: B, c: C, d: D) => IO<Z>;
+export function withEffectsP<A, B, C, D, E, Z>(f: F5<A, B, C, D, E, Promise<Z>>): (a: A, b: B, c: C, d: D, e: E) => IO<Z>;
+export function withEffectsP<A>(fn: (...as: any[]) => Promise<A>): (...a: any[]) => IO<A> {
+  return (...args: any[]) => liftF(new CallP(fn, args));
+}
+
+export function call<Z>(f: F0<Z>): IO<Z>;
+export function call<A, Z>(f: F1<A, Z>, a: A): IO<Z>;
+export function call<A, B, Z>(f: F2<A, B, Z>, a: A, b: B): IO<Z>;
+export function call<A, B, C, Z>(f: F3<A, B, C, Z>, a: A, b: B, c: C): IO<Z>;
+export function call<A, B, C, D, Z>(f: F4<A, B, C, D, Z>, a: A, b: B, c: C, d: D): IO<Z>;
+export function call<A, B, C, D, E, Z>(f: F5<A, B, C, D, E, Z>, a: A, b: B, c: C, d: D, e: E): IO<Z>;
+export function call(fn: Function, ...args: any[]): IO<any> {
+  return liftF(new Call(fn, args));
+}
+
+export function callP<Z>(f: F0<Z>): IO<Z>;
+export function callP<A, Z>(f: F1<A, Promise<Z>>, a: A): IO<Z>;
+export function callP<A, B, Z>(f: F2<A, B, Promise<Z>>, a: A, b: B): IO<Z>;
+export function callP<A, B, C, Z>(f: F3<A, B, C, Promise<Z>>, a: A, b: B, c: C): IO<Z>;
+export function callP<A, B, C, D, Z>(f: F4<A, B, C, D, Promise<Z>>, a: A, b: B, c: C, d: D): IO<Z>;
+export function callP<A, B, C, D, E, Z>(f: F5<A, B, C, D, E, Promise<Z>>, a: A, b: B, c: C, d: D, e: E): IO<Z>;
+export function callP(fn: Function, ...args: any[]): IO<any> {
+  return liftF(new CallP(fn, args));
+}
+
+export function throwE(error: any): IO<any> {
+  return liftF(new ThrowE(error));
+}
+
+export function catchE(
+  errorHandler: (error: any) => IO<any>, io: IO<any>
+): IO<any> {
+  return liftF(new CatchE(errorHandler, io));
+}
+
+export function doRunIO<A>(e: IO<A>): Promise<A> {
+  return e.match<Promise<A>>({
+    pure: (a) => Promise.resolve(a),
+    bind: (io, cont) => {
+      switch (io.type) {
+        case "call":
+          return runIO(cont(io.fn(...io.args)));
+        case "callP":
+          return io.fn(...io.args)
+            .then((a: A) => runIO(cont(a)));
+        case "catchE":
+          return doRunIO(io.io)
+            .then((a: A) => runIO(cont(a)))
+            .catch((err: any) => doRunIO(io.handler(err)));
+        case "throwE":
+          return Promise.reject(io.error);
+      }
+    }
+  });
+}
+
+export function runIO<A>(e: IO<A>): Promise<A> {
+  return doRunIO(e);
+}
+
+function doTestIO<A>(e: IO<A>, arr: any[], ending: A, idx: number): void {
+  e.match({
+    pure: (a2) => {
+      if (ending !== a2) {
+        throw new Error(
+          `Pure value invalid, expected ${ending} but saw ${a2}`
+        );
+      }
+    },
+    bind: (io, cont) => {
+      const [{ val: io2 }, a] = arr[idx];
+      if (!deepEqual(io, io2)) {
+        throw new Error(`Value invalid, expected ${io2} but saw ${io}`);
+      } else {
+        doTestIO(cont(a), arr, ending, idx + 1);
+      }
+    }
+  });
+}
+
+export function testIO<A>(e: IO<A>, arr: any[], a: A): void {
+  doTestIO(e, arr, a, 0);
+}
---
+++ test/index.ts
@@ -0,0 +1,220 @@
+import { assert } from "chai";
+import { go, ap } from "@funkia/jabz";
+
+import {
+  IO, runIO, testIO, withEffects, withEffectsP, call, callP, catchE, throwE
+} from "../src/index";
+
+function add(n: number, m: number) {
+  return n + m;
+}
+
+describe("IO", () => {
+  it("gives pure computation", () => {
+    return runIO(IO.of(12)).then((res) => {
+      assert.equal(12, res);
+    });
+  });
+  describe("functor", () => {
+    it("maps pure computation", () => {
+      return runIO(IO.of(12).map((n) => n * n)).then((res) => {
+        assert.equal(144, res);
+      });
+    });
+    it("is stack safe", () => {
+      const amount = 10000;
+      let mapped = IO.of(0);
+      for (let i = 0; i < amount; ++i) {
+        mapped = mapped.map((n) => n + 1);
+      }
+      // return runIO(mapped).then((n) => assert.strictEqual(n, amount));
+    });
+  });
+  it("chains computations", () => {
+    return runIO(IO.of(3).chain(n => IO.of(n + 4))).then((res) => {
+      assert.equal(7, res);
+    });
+  });
+  it("works with do-notation", () => {
+    const f1 = withEffects((a: number) => a * 2);
+    const f2 = withEffects((a: number, b: number) => a + b);
+    const comp: IO<number> = go(function* () {
+      const a = yield IO.of(4);
+      const b = yield f1(3);
+      const sum = yield f2(a, b);
+      return sum;
+    });
+    return runIO(comp).then((res) => {
+      assert.equal(10, res);
+    });
+  });
+  it("applies function in effects to value in other effects", () => {
+    const f1 = IO.of((a: number) => a * 2);
+    const f2 = IO.of(3);
+    const applied = ap(f1, f2);
+    return runIO(applied).then(res => assert.equal(res, 6));
+  });
+  describe("wrapping", () => {
+    it("wraps imperative function", () => {
+      let variable = 0;
+      function imperative(a: number, b: number): number {
+        variable = variable + a + b;
+        return variable;
+      }
+      const wrapped = withEffects(imperative);
+      const comp = go(function* () {
+        const a = yield wrapped(1, 2);
+        assert.strictEqual(variable, 3);
+        const b = yield wrapped(3, 4);
+        assert.strictEqual(variable, 10);
+        return a + b;
+      });
+      return runIO(comp).then((res) => {
+        assert.strictEqual(res, 13);
+      });
+    });
+    it("wraps imperative function returning promise", () => {
+      let variable = 0;
+      function imperativeP(a: number, b: number): Promise<number> {
+        variable = variable + a + b;
+        return Promise.resolve(variable);
+      }
+      const wrapped = withEffectsP(imperativeP);
+      const comp = go(function* () {
+        const a = yield wrapped(1, 2);
+        assert.strictEqual(a, 3);
+        assert.strictEqual(variable, 3);
+        const b = yield wrapped(3, 4);
+        assert.strictEqual(b, 10);
+        assert.strictEqual(variable, 10);
+        return add(a, b);
+      });
+      return runIO(comp).then((res) => {
+        assert.deepEqual(res, 13);
+      });
+    });
+  });
+  describe("error handling", () => {
+    const errorMessage = "I do not accept zero";
+    it("can catch error from rejected promise", () => {
+      function imperativeP(a: number): Promise<number> {
+        return a === 0
+          ? Promise.reject(errorMessage)
+          : Promise.resolve(a);
+      }
+      const wrapped = withEffectsP(imperativeP);
+      const comp = catchE((err: string) => IO.of(err.length), wrapped(0));
+      return runIO(comp).then((res) => {
+        assert.deepEqual(res, errorMessage.length);
+        return runIO(wrapped(0));
+      }).catch((res) => {
+        assert.deepEqual(res, errorMessage);
+      });
+    });
+    it("`catchE` function is not called when no error", () => {
+      return runIO(
+        catchE((_: any) => { throw new Error("No"); }, IO.of(12))
+      ).then((res) => {
+        assert.strictEqual(res, 12);
+      });
+    });
+    it("can throw error with `throwE`", () => {
+      const comp = catchE(
+        (err: string) => IO.of(err.length),
+        go(function* () {
+          const a = yield IO.of(13);
+          assert.deepEqual(a, 13);
+          const b = yield throwE(errorMessage);
+          return "Oh no, error thrown above >.<";
+        })
+      );
+      return runIO(comp).then((res) => {
+        assert.deepEqual(res, errorMessage.length);
+      });
+    });
+  });
+  describe("calling", () => {
+    it("calls function", () => {
+      let variable = 0;
+      function imperative(
+        a: number, b: number, c: number, d: number
+      ): number {
+        variable = a + b + c + d;
+        return variable;
+      }
+      return runIO(call(imperative, 1, 2, 3, 4)).then((res) => {
+        assert.strictEqual(variable, 10);
+        assert.strictEqual(res, 10);
+      });
+    });
+    it("calls promise returning function", () => {
+      let variable = 0;
+      function imperative(a: number, b: number): Promise<number> {
+        variable = a + b;
+        return Promise.resolve(variable);
+      }
+      return runIO(callP(imperative, 1, 2)).then((res) => {
+        assert.deepEqual(variable, 3);
+        assert.deepEqual(res, 3);
+      });
+    });
+    it("calls promise returning function that rejects", () => {
+      let variable = 0;
+      function imperative(a: number, b: number): Promise<number> {
+        variable = a + b;
+        return Promise.reject(variable);
+      }
+      return runIO(callP(imperative, 1, 2)).catch((res) => {
+        assert.deepEqual(variable, 3);
+        assert.deepEqual(res, 3);
+      });
+    });
+  });
+  describe("testing", () => {
+    let mutableN = 0;
+    function add(m: number) {
+      return mutableN += m;
+    }
+    function addTwice(m: number) {
+      return mutableN += 2 * m;
+    }
+    const wrapped1 = withEffects(add);
+    const wrapped2 = withEffects(addTwice);
+    it("can test without running side-effects", () => {
+      const comp = wrapped1(2).chain((n) => wrapped2(3));
+      testIO(comp, [
+        [wrapped1(2), 2],
+        [wrapped2(3), 8]
+      ], 8);
+      assert.deepEqual(mutableN, 0);
+    });
+    it("throws on incorrect function", () => {
+      const comp = wrapped1(2).chain((n) => wrapped2(3));
+      assert.throws(() => {
+        const expected = [
+          [call(wrapped2, 2), 2],
+          [call(wrapped2, 3), 8]
+        ];
+        testIO(comp, expected, 8);
+      });
+    });
+    it("handles computation ending with `of`", () => {
+      const comp = wrapped1(3).chain((n) => IO.of(4));
+      testIO(comp, [
+        [wrapped1(3), 3]
+      ], 4);
+      assert.throws(() => {
+        testIO(comp, [
+          [wrapped1(3), 3]
+        ], 5);
+      });
+    });
+    it("handles computation with map and chain", () => {
+      const comp2 = wrapped1(4).map((n) => n * n).chain((n) => wrapped2(n + 2));
+      testIO(comp2, [
+        [wrapped1(4), 4],
+        [wrapped2(18), 18 * 2 + 4]
+      ], 18 * 2 + 4);
+    });
+  });
+});
